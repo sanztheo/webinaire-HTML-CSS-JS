e65cf6a98fc870220cf56e61f16b558f9871b062
Sanzcloud-web <sanztheopro@gmail.com>
2026-02-27 17:12:22 +0100

    Enhance login form error handling in login.html and login.js by adding a dedicated error message display. Implement password and email validation logic to provide user feedback on incorrect credentials during form submission.

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -5,6 +5,23 @@
 // -- Récupération du formulaire
 const loginForm = document.getElementById("login-form");
 const passwordChamp = document.getElementById("login-password");
+const divError = document.getElementById("error");
+
+
+  
+
+// Checker Len password
+function validation(email, password) {
+	const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+	const isValidEmail = emailRegex.test(email);
+
+	if (password.length >= 6 && isValidEmail)  {
+		
+		return true;
+	} else {
+		return false;
+	}
+}
 
 // -------------------------------------------------------
 // Écouter le submit du formulaire
@@ -21,6 +38,10 @@ loginForm.addEventListener("submit", async (event) => {
 	const email = event.target.querySelector("[name=email]").value;
 	const password = event.target.querySelector("[name=password]").value;
 
+	if (!validation(email, password)) {
+		divError.textContent = "Email ou mot de passe incorrect ";
+		return;
+	}
 	const token = await envoyerLogin(email, password);
 
 	if (token) {
@@ -36,11 +57,7 @@ async function envoyerLogin(email, password) {
 		body: JSON.stringify({ email, password })
 	});
 	if (!response.ok) {
-		if (response.status === 401) {
-			alert("Email ou mot de passe incorrect");
-		} else {
-			alert("Erreur serveur");
-		}
+			divError.textContent = "Erreur server " + response.status;
 		return null;
 	}
 
